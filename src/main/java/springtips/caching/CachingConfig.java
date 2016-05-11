418e7757364204243b5d501daf7d5765e1db067f
Yann Cébron <yann.cebron@jetbrains.com>
2016-05-11 14:43:18 +0200

    CachingConfig: fix

--- src/main/java/springtips/caching/CachingConfig.java
+++ src/main/java/springtips/caching/CachingConfig.java
@@ -1,9 +1,11 @@
 package springtips.caching;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("springtips.caching.*")
+@EnableCaching
 public class CachingConfig {
 }
